a831cedfc31d6b29707d5749d0980b8a184df3d8
Rostislav Rabiec <112706430+Rostislav87@users.noreply.github.com>
2024-08-07 10:16:22 +0200

    Add files via upload

---
+++ company_data.py
@@ -0,0 +1,13 @@
+
+data = {'Company_name': ['Berkshire Hathaway'],
+        'Symbol': ['BRK-B'],
+        'Sector': ['Finance'],
+        'Industry': ['Property, Casualty and Title Insurance'],
+        'CEO' :['Warren E. Buffet']
+                }
+
+
+
+
+
+
